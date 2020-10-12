51747c29eecdae60737d778e84d1544c4af835c4
vasu <vanka.vasu@gmail.com>
2020-10-12 15:27:19 +0530

    added mute/unmute

--- src/app/components/countdowner/countdowner.component.ts
+++ src/app/components/countdowner/countdowner.component.ts
@@ -13,21 +13,20 @@ export class CountdownerComponent {
   percent: number = 0;
   private timeRef: any = null;
   time: string = '';
+  muted: boolean = false;
   constructor() { }
 
   async start() {
     if (!this.timerControl.valid) {
       return;
     }
-    await this.playSound('assets/sounds/bing_bong.mp3');
+    await this.playSound('assets/sounds/init.mp3');
     this.inprogress = true;
     const timeInSec = this.timerControl.value * 60;
     let timeleft = timeInSec;
     this.timeRef = setInterval(async () => {
       this.percent = Math.round(100 - ((Math.round(timeleft) / timeInSec) * 100));
-      let minutes = Math.floor(timeleft / 60);
-      let seconds = Math.floor(timeleft % 60);
-      this.time = `${minutes}:${seconds}`;
+      this.time = `${new Date(timeleft * 1000).toISOString().substr(11, 8)}`;
       if (timeleft <= 0) {
         clearInterval(this.timeRef);
         await this.playSound('assets/sounds/time_up.mp3');
@@ -40,7 +39,9 @@ export class CountdownerComponent {
   private async playSound(location: string) {
     var audio = new Audio(location);
     audio.loop = false;
-    await audio.play();
+    if (!this.muted) {
+      await audio.play();
+    }
   }
 
   reset() {
@@ -52,5 +53,9 @@ export class CountdownerComponent {
     this.inprogress = false;
     this.percent = 0;
     this.time = '';
+  }
+
+  disableStart(): boolean {
+    return this.inprogress || !this.timerControl.valid;
   }
 }
